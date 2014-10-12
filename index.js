5f5f63190a7d6e7da90a37b48297f576c80f1d4f
Nolan Lawson <nolan.lawson@gmail.com>
2014-10-12 19:20:20 -0400

    http vs https okay

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ var yargs = require('yargs')
   .describe('u', 'username for the CouchDB database (if it\'s protected)')
   .alias('p', 'password')
   .describe('p', 'password for the CouchDB database (if it\'s protected)')
-  .demand(1, 'You need to supply a database URL or filepath')
   .example('$0 http://localhost:5984/mydb > dump.txt',
     'Dump from the "mydb" CouchDB to dump.txt')
   .example('$0 /path/to/mydb > dump.txt',
@@ -29,6 +28,12 @@ if (argv.h) {
   return process.exit(0);
 }
 
+var dbName = argv._[0];
+if (!dbName) {
+  console.error('You need to supply a database URL or filepath. -h for help');
+  return process.exit(1);
+}
+
 var Promise = require('lie');
 var PouchDB = require('pouchdb');
 var replicationStream = require('pouchdb-replication-stream');
@@ -39,7 +44,6 @@ Object.keys(replicationStream.adapters).forEach(function (adapterName) {
   PouchDB.adapter(adapterName, adapter);
 });
 
-var dbName = argv._[0];
 var outfile = argv.o;
 var password = argv.p;
 var username = argv.u;
@@ -49,23 +53,22 @@ if ((!!password) !== (!!username)) {
   return process.exit(1);
 } else if (password) {
   var URL = require('url');
-  var parsedURL;
-  try {
-    parsedURL = URL.parse(dbName);
-  } catch (err) {
-    console.error(err);
+  var parsedURL = URL.parse(dbName);
+  if (!parsedURL.protocol) {
     console.error('Usernames/passwords are only for remote databases');
+    console.error('Is ' + dbName + ' a remote database?');
     return process.exit(1);
   }
-  dbName = parsedURL.scheme + '://' + encodeURIComponent(username) +
+  dbName = parsedURL.protocol + '//' + encodeURIComponent(username) +
     ':' + encodeURIComponent(password) + '@' + parsedURL.host +
     parsedURL.path;
 }
 
 // check that it exists
 return new Promise(function (resolve, reject) {
-  if (/^http:\/\//.test(dbName)) {
-    require('http').get(dbName, function (res) {
+  if (/^https?:\/\//.test(dbName)) {
+    var protocol = /^https/.test(dbName) ? require('https') : require('http');
+    protocol.get(dbName, function (res) {
       if (res.statusCode && res.statusCode / 100 === 2) {
         resolve();
       } else {
@@ -73,6 +76,7 @@ return new Promise(function (resolve, reject) {
         reject(new Error(dbName + ': ' + res.statusCode));
       }
     }).on('error', reject).end();
+    resolve();
   } else {
     require('fs').exists(dbName, function (exists) {
       if (!exists) {
